c028c58f0dee370d5f9f8c1119c25c36eb154501
Mona-Nabil <mbellanbil@gmail.com>
2024-03-13 16:30:00 +0200

    first-commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "15c1aafef1mshc70b81af055ba09p185897jsn2a437314964c",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "15bc6be7306066d2e655ae1eddc5547c";
